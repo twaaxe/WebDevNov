7ad3c3935b87d2604ada710dce1530944f4bea0f
twaaxe <twaaxe@gmail.com>
2023-03-27 15:56:37 +0200

    react

--- Reactjs/tutorial1/tuto1/src/App.js
+++ Reactjs/tutorial1/tuto1/src/App.js
@@ -6,13 +6,39 @@ import Newsfeed from './component/newsfeed'
 
 
 function App() {
+  const handleInput = (e) => {
+    console.log(e.key) // afche lettre pat lettre
+    console.log(e.target.value) // += e.value
+  }
   return (
     <div>
       <Header/>
       <Newsfeed/>
+      <input type='text' onKeyDown={handleInput}/>
+    {/* {
+        for(let x = 0; x<10;x++){
+        }
+    } */}
+    {/* forEach or map other array methods */}
+    {/* {
+        if(true){
+            console.log('blabla')
+        }
+    } */}
+
+    {
+        true? 'yes':'no'
+    }
       <Footer/>
     </div>
   )
 }
 
 export default App
+
+
+
+const handleInput = (e) => {
+  console.log(e.key) // afche lettre pat lettre
+  console.log(e.target.value) // += e.value
+}
--- Reactjs/tutorial1/tuto1/src/component/newsfeed.js
+++ Reactjs/tutorial1/tuto1/src/component/newsfeed.js
@@ -1,10 +1,22 @@
 function Newsfeed(){
+    const handleclick = ()=>console.log('ONCLICK')
+    const mouseOver = ()=>alert('ONMOUSEOVER')
+    const handleNewsData = ()=>{
+        fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(res => res.json())
+        .then(data => console.log(data))
+    }
+    
+
+
+
+
     return(
 
         <ul>
-            <li>news1</li>
-            <li>news2</li>
-            <li>news3</li>
+            <li onClick={handleclick}>news1</li>
+            <li onMouseOver={mouseOver}>news2</li>
+            <li onClick={handleNewsData}>news3</li>
             <li>news4</li>
         </ul>
 
