8c46c1ffb1f9963c54457bdeedb3a9cac418662f
twaaxe <35602018+twaaxe@users.noreply.github.com>
2023-03-29 21:21:04 +0200

    shopping list exo

--- Reactjs/shoppinglist/src/component/header.js
+++ Reactjs/shoppinglist/src/component/header.js
@@ -1,24 +1,27 @@
+import '../style/style.css'
+
+
 export default function Header() {
     return(
         <>
          
-            <nav class="navbar navbar-expand-md navbar-dark bg-dark">
-                <div class="container">
-                    <a class="navbar-brand" href="#"><i class="fas fa-warehouse me-2"></i>1337's Warehouse</a>
-                    <button class="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
+            <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+                <div className="container">
+                    <a className="navbar-brand" href="#"><i className="fas fa-warehouse me-2"></i>1337's Warehouse</a>
+                    <button className="navbar-toggler d-lg-none" type="button" data-bs-toggle="collapse"
                         data-bs-target="#collapsibleNavId" aria-controls="collapsibleNavId" aria-expanded="false"
                         aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
+                        <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div class="collapse navbar-collapse" id="collapsibleNavId">
-                        <ul class="navbar-nav me-auto mt-2 mt-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link" href="#" aria-current="page">Products <span
-                                        class="visually-hidden">(current)</span></a>
+                    <div className="collapse navbar-collapse" id="collapsibleNavId">
+                        <ul className="navbar-nav me-auto mt-2 mt-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link" href="#" aria-current="page">Products <span
+                                        className="visually-hidden">(current)</span></a>
                             </li>
-                            <li class="nav-item">
-                                <a class="nav-link" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo" href="#" aria-current="page">Summary Cart <span
-                                        class="visually-hidden">(current)</span></a>
+                            <li className="nav-item">
+                                <a className="nav-link" data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@mdo" href="#" aria-current="page">Summary Cart <span
+                                        className="visually-hidden">(current)</span></a>
                             </li>
                         </ul>
 
