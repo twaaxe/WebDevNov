54021f9b603e6ae72254be2d58a388df42c44e11
twaaxe <twaaxe@gmail.com>
2023-03-30 14:47:10 +0200

    fullWS header footer

---
+++ Reactjs/fullwebsite/src/component/Header.js
@@ -0,0 +1,13 @@
+import Navigation from "./Navigation";
+
+
+export default function Header(){
+
+    return(
+        <>
+        <Navigation />
+        
+        </>
+    )
+
+}
---
+++ Reactjs/fullwebsite/src/component/Navigation.js
@@ -0,0 +1,17 @@
+export default function Navigation (){
+
+
+    return(
+        <>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/Blog">Blog</a></li>
+                <li><a href="/Contact">Contact</a></li>
+                <li><a href="/About">About</a></li>
+            </ul>
+            <h1>This header from Navigation</h1>
+        </>
+    )
+}
+
+//va aller dans HEADER
