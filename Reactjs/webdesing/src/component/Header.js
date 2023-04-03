140ef7d62ae4d71e56919293e6f2c3b20beb5e1a
twaaxe <35602018+twaaxe@users.noreply.github.com>
2023-04-03 19:54:45 +0200

    webdesign layout done no color

--- Reactjs/webdesing/src/component/Header.js
+++ Reactjs/webdesing/src/component/Header.js
@@ -1,34 +1,34 @@
-import {Row,Col, Nav, Navbar, Container} from 'react-bootstrap';
+import { Row, Col, Nav, Navbar, Container } from 'react-bootstrap';
 
-export default function Header(){
+export default function Header() {
 
-    return(
-    //     <Row className="my-3 p-2">
-    //         <Col md={3} className=" justify-content-start  text-center headerButton">
-    //             <div id="abouKero">About Kero</div>
-    //         </Col>
-    //         <Col md={9} className=" justify-content-around headerButton" >
-    //             <button>user inteface</button>
-    //             <div>web design</div>
-    //             <div>programming</div>
-    //         </Col>
-    //     </Row>
+    return (
+        //     <Row className="my-3 p-2">
+        //         <Col md={3} className=" justify-content-start  text-center headerButton">
+        //             <div id="abouKero">About Kero</div>
+        //         </Col>
+        //         <Col md={9} className=" justify-content-around headerButton" >
+        //             <button>user inteface</button>
+        //             <div>web design</div>
+        //             <div>programming</div>
+        //         </Col>
+        //     </Row>
 
-    <Navbar bg="dark" variant="dark">
-        <Container >
-           
-                
+        <Navbar bg="dark" variant="dark">
+
+
+
             <Navbar.Brand href="#home">About Kero</Navbar.Brand>
-                
-                
-            <Nav className=" ms-auto">
+
+
+            <Nav className=" mx-auto d-flex justify-content-evenly">
                 <Nav.Link href="#home">User Inerface</Nav.Link>
                 <Nav.Link href="#features">Web Design</Nav.Link>
                 <Nav.Link href="#pricing">Programming</Nav.Link>
             </Nav>
-                
-            
-        </Container>
-    </Navbar>
+
+
+
+        </Navbar>
     )
 }
