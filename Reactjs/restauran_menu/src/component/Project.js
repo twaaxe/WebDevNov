1fff4805c1bb2af0899891174ff445d379d20238
twaaxe <twaaxe@gmail.com>
2023-04-04 15:48:37 +0200

    webdesing

---
+++ Reactjs/restauran_menu/src/component/Project.js
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+export default function Project(){
+
+    const {id} = useParams()
+    useEffect(()=>{
+
+
+    }, [] )//empty array to avoid infinite refresh
+}
+
+
+
