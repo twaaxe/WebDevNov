2b75812da0338f7115c2c1502fbeef867cadb1bd
twaaxe <35602018+twaaxe@users.noreply.github.com>
2023-04-28 15:23:51 +0200

    update

--- Reactjs/shoppinglist/src/App.js
+++ Reactjs/shoppinglist/src/App.js
@@ -2,24 +2,24 @@
 
 
 function App() {
-  
-  return(
-    <main className="my-3">
-    <div className="container-md">
 
-        <div className="row my-2">
-            <div className="col-md-6">
-                <div className="input-group">
-                    <input type="text" className="form-control searchinput" placeholder="Search products"/>
-                    <button className="btn btn-primary searchbtn" type="submit">Search</button>
+    return (
+        <main className="my-3">
+            <div className="container-md">
+
+                <div className="row my-2">
+                    <div className="col-md-6">
+                        <div className="input-group">
+                            <input type="text" className="form-control searchinput" placeholder="Search products" />
+                            <button className="btn btn-primary searchbtn" type="submit">Search</button>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
 
-       
-        <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4 products">
 
-            {/* <div className="col">
+                <div className="row row-cols-1 row-cols-md-2 row-cols-lg-4 g-4 products">
+
+                    {/* <div className="col">
                 <div className="card h-100 bg-secondary text-light">
                     <img src="https://i.dummyjson.com/data/products/6/1.png" className="product-image"
                         alt="Product Image"/>
@@ -31,16 +31,16 @@ function App() {
                 </div>
             </div> */}
 
-            
-
-        </div>
 
 
-    </div>
+                </div>
 
-</main>
-  )
-   
+
+            </div>
+
+        </main>
+    )
+
 }
 
 export default App;
